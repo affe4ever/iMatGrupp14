82b6018a5ada080761ed42629f85d6380fb12c7a
Alfred <alfred.berglof@gmail.com>
2022-05-06 10:46:24 +0200

    v.1

---
+++ resources/main/java/iMatApplication.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class iMatApplication extends Application{
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("iMatHomePage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("iMat");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args){
+        launch();
+    }
+}
